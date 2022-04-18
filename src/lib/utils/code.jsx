c2c981b5e1eee750f3a82bf71cc9fe77416c4642
Yannik <ywojcicki@mailbox.org>
2022-04-18 00:20:23 -0700

    Add more snippets

--- src/lib/utils/code.jsx
+++ src/lib/utils/code.jsx
@@ -7,6 +7,11 @@ const SNIPPETS = [
     '\treturn product;\n' +
     '};',
   'const factorial = number => {\n' + '\treturn number < 2 ? 1 : number * factorial(number - 1);\n' + '};',
+  "const cookie = name => `; ${document.cookie}`.split(`; ${name}=`).pop().split(';').shift();",
+  'const rgbToHex = (r, g, b) =>\n' + "'#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);\n" + 'rgbToHex(0, 51, 255); ',
+  'const copyToClipboard = (text) => navigator.clipboard.writeText(text);',
+  'const isDateValid = (...val) => !Number.isNaN(new Date(...val).valueOf());',
+  'const dayOfYear = (date) => \n' + 'Math.floor((date - new Date(date.getFullYear(), 0, 0)) / 1000 / 60 / 60 / 24;',
 ];
 
 export const getRandomCodeSnippet = () => {
