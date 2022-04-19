4181dc37b866b9a587227ac284453ce0ec8edf30
Yannik <ywojcicki@mailbox.org>
2022-04-19 00:11:19 -0700

    Add amplitude

---
+++ src/lib/utils/analytics.js
@@ -0,0 +1,34 @@
+import amplitude from 'amplitude-js';
+import { detect } from 'detect-browser';
+
+const apiKey = process.env.NEXT_PUBLIC_AMPLITUDE_KEY;
+const env = process.env.NODE_ENV;
+
+const initAnalytics = () => {
+  console.log('[Init Analytics]');
+  amplitude.getInstance().init(apiKey || '', undefined, {
+    saveEvents: true,
+    includeUtm: true,
+    includeReferrer: true,
+  });
+};
+
+const track = (name, props = {}) => {
+  console.log('name, props', name, props);
+  const browser = detect();
+  console.log('browser', browser);
+
+  const updatedProps = {
+    ...props,
+    browser: browser && browser.name ? browser.name.toLowerCase() : 'unknown',
+  };
+
+  // if (env === 'production') {
+  amplitude.getInstance().logEvent(name, updatedProps);
+  // } else {
+  //   // eslint-disable-next-line no-console
+  //   console.info(`[${env}] Logging event to Amplitude`, { name, props: updatedProps });
+  // }
+};
+
+export { initAnalytics, track };
